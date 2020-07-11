667592872ce320a2a35626dd043fa961158b5d47
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-07-11 14:29:13 -0700

    changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,5 @@
 const path = require("path");
 
-//const calendar = require("../public/assets/js/calendar")
-
-// path.get("/calendar", function(req, res) {
-//     calendar.all(function(data) {
-//         var handlebars = {
-//             calendar:data
-//         };
-//         console.log(handlebars)
-//         res.render("index", handlebars)
-//     })
-// })
-
-// router.get("/", function(req, res) {
-//     cat.all(function(data) {
-//       var hbsObject = {
-//         cats: data
-//       };
-//       console.log(hbsObject);
-//       res.render("index", hbsObject);
-//     });
-//   });
-
-
-
 module.exports = function(app){
     app.get("/", function(req, res){
         if(req.user){
