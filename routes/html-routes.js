760dbb26bce341d4c42d8c99a754786ffec3c314
Tyler Ink Broeker <tylerinkbroeker@gmail.com>
2020-07-11 14:12:40 -0700

    set up calendar handlebars and route to render it

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,10 +1,35 @@
-var path = require("path");
+const path = require("path");
+
+//const calendar = require("../public/assets/js/calendar")
+
+// path.get("/calendar", function(req, res) {
+//     calendar.all(function(data) {
+//         var handlebars = {
+//             calendar:data
+//         };
+//         console.log(handlebars)
+//         res.render("index", handlebars)
+//     })
+// })
+
+// router.get("/", function(req, res) {
+//     cat.all(function(data) {
+//       var hbsObject = {
+//         cats: data
+//       };
+//       console.log(hbsObject);
+//       res.render("index", hbsObject);
+//     });
+//   });
+
+
 
 module.exports = function(app){
     app.get("/", function(req, res){
         if(req.user){
             res.redirect("/user");
         }
-        res.sendFile(path.join(__dirname, "../views/calendar.html"))// needs handlebars page
+        res.render("calendar")
+        //res.sendFile(path.join(__dirname, "../views/layouts/main"))// needs handlebars page
     });
 }
