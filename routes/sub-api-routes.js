0b68dee90a9c77295e13e4e3a6bd187be4cc37eb
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-07-13 21:00:50 -0700

    subscription api route

--- routes/sub-api-routes.js
+++ routes/sub-api-routes.js
@@ -4,15 +4,55 @@ module.exports = function(app){
 console.log("sub route api")
 app.get("/api/bills",function(req,res){
     var subs = {};
-    if (req.subs){
-        subs = req.subs;
+    if (req.subs.user_id){
+        subs.UserId = req.subs.user_id;
     }
     db.Subscription.findAll({
         where: subs,
         include: [db.User],
-    }).then(function(dbSub){
-        res.json(dbSub)
+    }).then(function(dbSubs){
+        res.json(dbSubs)
     });
 });
+app.get("/api/subs/:id", function(req,res){
+    db.Subscription.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [db.User]
+    }).then(function(dbPost){
+        res.json(dbSubs)
+    });
+});
+
+app.post("/api/subs",function(req, res){
+    db.Post.create(req.body).then(function(dbSubs){
+        res.json(dbSubs);
+    });
+});
+
+app.delete("/api/subs",function(req,res){
+    db.Subscription.update(
+        req.body,
+        {
+            where: {
+                id: req.body.id
+            }
+        }).then(function(dbSubs){
+            res.json(dbSubs);
+        });
+});
+
+app.put("/api/subs",function(req,res){
+    db.Subscription.update(
+        req.body,
+        {
+            where: {
+                id: req.body.id
+            }
+        }).then(function(dbSubs){
+            res.json(dbSubs)
+        })
+})
 
 }
