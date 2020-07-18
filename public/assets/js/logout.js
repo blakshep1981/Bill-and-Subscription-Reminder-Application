d4e27846c66f7b067bce7973adfa63b486dee00f
SpaceDandyA80 <58712760+SpaceDandyA80@users.noreply.github.com>
2020-07-18 03:54:57 -0700

    changes

--- public/assets/js/logout.js
+++ public/assets/js/logout.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    console.log("inside logout.js");
+    //console.log("inside logout.js");
 
     $('#btn-logout').on('click', function (e) {
         // Prevent Default Submit Event
         e.preventDefault();
 
-        console.log("Performing submit");
+       // console.log("Performing submit");
 
 
 
